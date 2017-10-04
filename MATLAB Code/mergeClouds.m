e6c81487e9b088db5d2273711e5646713fd10cbc
Nagrajan23 <c.raj.23@gmail.com>
2017-10-04 12:48:36 -0400

    bug fix

--- MATLAB Code/mergeClouds.m
+++ MATLAB Code/mergeClouds.m
@@ -4,6 +4,6 @@ pcOld = pointCloud(readXYZ(lidarData(1)),'Intensity',readField(lidarData(1),'int
 for i = 2:pcLen
     pcTemp = pointCloud(readXYZ(lidarData(i)),'Intensity',readField(lidarData(i),'intensity'));
     pcOut = pcmerge(pcTemp, pcOld, 0.0001);
-    pcOld = pcTemp;
+    pcOld = pcOut;
 end
 pcshow(pcOut)
