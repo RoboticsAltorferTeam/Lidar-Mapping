d935b2b0b1ae5cd3657c79625fbabb2c61038696
Nagrajan23 <c.raj.23@gmail.com>
2017-10-10 16:06:50 -0400

    code

--- MATLAB Code/mergeClouds.m
+++ MATLAB Code/mergeClouds.m
@@ -1,17 +1,21 @@
 
-[~,pcLen] = size(lidarData);
+[pcLen,~] = size(lidarData);
+% pcLen = 252;
 % pcOld = pointCloud(readXYZ(lidarData(1)),'Intensity',readField(lidarData(1),'intensity'));
 % for i = 2:pcLen
 %     pcTemp = pointCloud(readXYZ(lidarData(i)),'Intensity',readField(lidarData(i),'intensity'));
 %     pcOut = pcmerge(pcTemp, pcOld, 0.0001);
 %     pcOld = pcOut;
 % end
+pcCount = 1;
 
-pcObj{1} = pointCloud(readXYZ(lidarData(1)),'Intensity',readField(lidarData(1),'intensity'));
+pcObj{pcCount} = pointCloud(readXYZ(lidarData{1}),'Intensity',readField(lidarData{1},'intensity'));
+pcCount = pcCount + 1;
 for i = 2:pcLen
-    pcObj{i} = pointCloud(readXYZ(lidarData(i)),'Intensity',readField(lidarData(i),'intensity'));
-    pcOut = pcmerge(pcObj{i}, pcObj{i-1}, 0.0001);
+    i
+    pcObj{pcCount} = pointCloud(readXYZ(lidarData{i}),'Intensity',readField(lidarData{i},'intensity'));
+    pcCount = pcCount + 1;
+%     pcOut = pcmerge(pcObj{i}, pcObj{i-1}, 0.0001);
 %     pcOld = pcOut;
 end
-
-pcshow(pcOut)
+% pcshow(pcOut)
